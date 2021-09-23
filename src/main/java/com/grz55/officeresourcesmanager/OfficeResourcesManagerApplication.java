da4944eee17324253c55b8646131a0b82e213c29
nowakowskig1 <grzegorz.l.nowakowski@comarch.pl>
2021-09-23 18:04:24 +0200

    Initial commit

---
+++ src/main/java/com/grz55/officeresourcesmanager/OfficeResourcesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.grz55.officeresourcesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeResourcesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeResourcesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grz55/officeresourcesmanager/OfficeResourcesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grz55.officeresourcesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeResourcesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
